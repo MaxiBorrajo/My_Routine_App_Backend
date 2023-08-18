918311ade7a7b89650aa30fbbe9d539e66f429df
MaxiBorrajo <maximilianoborrajo0@gmail.com>
2023-08-18 11:00:49 -0300

    trying deploy

--- server.js
+++ server.js
@@ -45,6 +45,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+  });
+
+  next();
+});
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
